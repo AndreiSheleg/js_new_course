551e5d88df3bc0e5f8241c5922251516166a48aa
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-09-19 17:39:56 +0300

    init commit

---
+++ 03-boolean-if-else/03.js
@@ -0,0 +1,19 @@
+let word0 = 'Engineer'
+let word0translation = 'Инженер'
+
+let word1 = 'Ice Cream'
+let word1translation = 'Мороженое'
+
+let word2 = 'Milk'
+let word2translation = 'Молоко'
+
+alert(word0)
+alert(word0translation)
+
+alert(word1)
+alert(word1translation)
+
+alert(word2)
+alert(word2translation)
+
+alert('ФИНИШ - конец словаря')
